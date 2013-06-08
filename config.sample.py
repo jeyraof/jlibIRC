635d89d6862fa710de70c144f5c247d7687c0fa3
Jaeyoung Lee <jaeyoung@monodiary.net>
2013-06-09 08:43:33 +0900

    first commit

---
+++ config.sample.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+## NOTE: Save as 'config.py'
+
+SETTING = {
+    'HOST': "irc.ozinger.org", # Host of irc server
+    'PORT': 6667, # Port to connection
+    'IDEN': "", # Identity
+    'REAL': "", # Real name
+    'NICK': "", # Nick name
+    'CHAN': ["#freyja"], # Auto join channel list (add prefix '#' to channel name)
+    'NSRV': "Nickserv", # Nick service bot Nick name
+    'NSID': "", # Nick service auth ID
+    'NPWD': "" # Nick service auth Password
+}
